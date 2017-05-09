f364e5f339b1dcb09a0e57fd2503d50cb6a93bff
Kurt Lewis <kurt.jlewis15@gmail.com>
2017-05-08 21:55:55 -0400

    Save history to preferences file!

--- app/src/main/java/com/kurtjlewis/learning_android/DisplayMessageActivity.java
+++ app/src/main/java/com/kurtjlewis/learning_android/DisplayMessageActivity.java
@@ -1,6 +1,8 @@
 package com.kurtjlewis.learning_android;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -19,5 +21,18 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextVieew and set the string as its text
         TextView textView1 = (TextView) findViewById(R.id.textView1);
         textView1.setText(message);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        String message = (String)textView1.getText();
+
+        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(getString(R.string.saved_history_1), message);
+        editor.commit();
     }
 }
--- app/src/main/java/com/kurtjlewis/learning_android/HistoryFragment.java
+++ app/src/main/java/com/kurtjlewis/learning_android/HistoryFragment.java
@@ -1,10 +1,13 @@
 package com.kurtjlewis.learning_android;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by kurt on 5/7/17.
@@ -16,6 +19,16 @@ public class HistoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return  inflater.inflate(R.layout.fragment_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_history, container, false);
+
+        // Get history entry
+        SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+        String defaultHistory = getResources().getString(R.string.history_placeholder);
+        String history = sharedPref.getString(getString(R.string.saved_history_1), defaultHistory);
+
+        TextView historyText = (TextView) view.findViewById(R.id.historyTextView);
+        historyText.setText(history);
+
+        return view;
     }
 }
